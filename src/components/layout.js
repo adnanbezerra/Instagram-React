bdcd26a930112f4bad6cf3b46c8d124c2f241e87
adnan <adnanbezerra@gmail.com>
2022-05-05 20:00:30 -0300

    configuração do ambiente e criação da navbar

---
+++ src/components/layout.js
@@ -0,0 +1,7 @@
+import Navbar from "./navbar"
+
+export default function Layout() {
+    return (
+        <Navbar />
+    )
+}
---
+++ src/index.js
@@ -0,0 +1,4 @@
+import reactDom from "react-dom";
+import Layout from "./components/layout";
+
+reactDom.render(<Layout />, document.querySelector(".root"));
