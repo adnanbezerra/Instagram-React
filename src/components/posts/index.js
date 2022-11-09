ee6592f3bb00ef494ae3a2ff47d35b20b63f1560
seu nome <adnanbezerra@gmail.com>
2022-11-09 20:12:20 -0300

    refactor: change images imports again

--- src/components/posts/index.js
+++ src/components/posts/index.js
@@ -1,12 +1,33 @@
-import Topo from "./topo";
-import Card from "./card";
+import Post from "./post";
+import meowed from '../../assets/images/meowed.svg';
+import gatoTelefone from '../../assets/images/gato-telefone.svg';
+import respondeAi from '../../assets/images/respondeai.svg';
+import barked from '../../assets/images/barked.svg';
+import dog from '../../assets/images/dog.svg';
+import adorableAnimals from '../../assets/images/adorable_animals.svg';
 
-export default function Post(props) {
+export default function Posts() {
+    const posts = [
+        {
+            posterName: "meowed",
+            posterImage: meowed,
+            postImg: gatoTelefone,
+            likerUrl: respondeAi,
+            likerName: "respondeai",
+            likesNumber: "101.523"
+        },
+        {
+            posterName: "barked",
+            posterImage: barked,
+            postImg: dog,
+            likerUrl: adorableAnimals,
+            likerName: "adorable_animals",
+            likesNumber: "99.159"
+        }
+    ]
     return (
-        <div class="post">
-            <Topo user={props.posterName} url={props.posterImage} />
-
-            <Card postUrl={props.postImg} likerUrl={props.likerUrl} likerName={props.likerName} likesNumber={props.likesNumber} />
+        <div class="posts">
+            {posts.map((post) => <Post posterName={post.posterName} posterImage={post.posterImage} postImg={post.postImg} likerUrl={post.likerUrl} likerName={post.likerName} likesNumber={post.likesNumber} />)}
         </div>
     )
 }
